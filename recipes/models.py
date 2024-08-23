a392158586739bcbf1fd9e8a20a810dc53f06f46
Sophie Tigerholm <sophie.tigerholm@gmail.com>
2024-08-23 08:03:34 +0000

    Add Recipe class and model to database

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+SERVINGS = [tuple([x, x]) for x in range(1, 10)]
+STATUS = ((0, "Draft"), (1, "Published"))
+
+class Recipe(models.Model):
+    """
+    Stores a Recipe post entered by admin related to :model:`auth.User`.
+    """
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    description = models.TextField(max_length=300)
+    author = models.ForeignKey(User, on_delete=models.PROTECT,
+        limit_choices_to={'is_staff': True}, related_name="recipe_posts")
+    servings = models.IntegerField(choices=SERVINGS)
+    calories = models.CharField(max_length=10)
+    protein = models.CharField(max_length=10)
+    carbs = models.CharField(max_length=10)
+    fat = models.CharField(max_length=10)
+    ingredients = models.TextField()
+    instructions = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    def __str__(self):
+        return self.title
+
+
+
