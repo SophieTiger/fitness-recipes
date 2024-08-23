13e485a44b2dcb04569da2a8b52cff7c66870dbb
Sophie Tigerholm <sophie.tigerholm@gmail.com>
2024-08-23 06:55:57 +0000

    Add secret key and deploy

--- fitnessrecipes/settings.py
+++ fitnessrecipes/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q6!3w0!tbgq*2!v_b%2@-gi#^*5$-q0mf!8r^)k(_k+uemw*v!'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
