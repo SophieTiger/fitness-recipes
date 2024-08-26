8dca86d13364b9052c2690bafad940c53d1c22d9
Sophie Tigerholm <sophie.tigerholm@gmail.com>
2024-08-26 12:33:32 +0000

    Install summernote and add to requirements

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
-from .models import Recipe
-from .models import Comment
+from .models import Recipe, Comment
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Recipe)
+class RecipeAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'slug', 'status')
+    search_fields = ['title']
+    list_filter = ('status',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('ingredients', 'instructions')
 
 # Register your models here.
-admin.site.register(Recipe)
 admin.site.register(Comment)
