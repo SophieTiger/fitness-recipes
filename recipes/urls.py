ab6576a84f844b56f792603e661a3f9026134d96
Sophie Tigerholm <sophie.tigerholm@gmail.com>
2024-08-27 09:40:55 +0000

    Add url for recipes list by tag

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.RecipeList.as_view(), name='recipes')
+    path('', views.RecipeList.as_view(), name='recipes'),
+    path('tag/<slug:tag_slug>/', views.RecipeList.as_view(), name='recipes_list_by_tag'),
 ]
