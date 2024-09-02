5aab4271137162e9907153cea5763ad761dd6eec
Sophie Tigerholm <sophie.tigerholm@gmail.com>
2024-09-02 14:02:16 +0000

    Wire up post request on contact form and add to admin site

--- core/views.py
+++ core/views.py
@@ -1,15 +1,25 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import About
 from .forms import ContactForm
+
 
 # Create your views here.
 def about_fitness_recipes(request):
     """
-    Renders the About page
+    Renders the About page and Contact form
     """
+    if request.method == "POST":
+        contact_form = ContactForm(data=request.POST)
+        if contact_form.is_valid():
+            contact_form.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Thank you for the message! I will get back to you within 24 hours.')
+
     about = About.objects.all().order_by('-updated_on').first()
     contact_form = ContactForm()
-
+    
     return render(
         request,
         "about/about.html",
