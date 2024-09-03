c0f070842139401337b46efddd813e1050485925
Sophie Tigerholm <sophie.tigerholm@gmail.com>
2024-09-03 09:42:32 +0000

    Add view, url, html and javascript to update comments

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.RecipeList.as_view(), name='recipes'),
     path('tag/<slug:tag_slug>/', views.RecipeList.as_view(), name='recipes_list_by_tag'),
     path('<slug:slug>/', views.recipe_detail, name='recipe_detail'),
+    path('<slug:slug>/edit_comment/<int:comment_id>',
+         views.comment_edit, name='comment_edit'),
 ]
