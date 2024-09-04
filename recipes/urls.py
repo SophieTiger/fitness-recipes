6b753a1e74f349f4f587dc9f85fc45756f15de15
Sophie Tigerholm <sophie.tigerholm@gmail.com>
2024-09-04 13:30:39 +0000

    Add view and url for liked recipes

--- recipes/urls.py
+++ recipes/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
          views.comment_edit, name='comment_edit'),
     path('<slug:slug>/delete_comment/<int:comment_id>',
          views.comment_delete, name='comment_delete'),
+    path('recipe/<slug:slug>/like/', views.like_recipe, name='like_recipe'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic
 from django.contrib import messages
-from django.http import HttpResponseRedirect
-from .models import Recipe, Comment
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect, JsonResponse
+from .models import Recipe, Comment, Like
 from .forms import CommentForm
 from taggit.models import Tag
 
@@ -56,6 +57,8 @@ def recipe_detail(request, slug):
     recipe = get_object_or_404(queryset, slug=slug)
     comments = recipe.comments.all().order_by("-created_on")
     comment_count = recipe.comments.filter(approved=True).count()
+
+     # Handle comment form submission
     if request.method == "POST":
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
@@ -70,6 +73,11 @@ def recipe_detail(request, slug):
     
     comment_form = CommentForm()
 
+    # Likes functionality
+    likes_count = Like.objects.filter(recipe=recipe).count()
+    user_liked = request.user.is_authenticated and Like.objects.filter(
+        user=request.user, recipe=recipe).exists()
+
     return render(
         request,
         "recipes/recipe_detail.html",
@@ -77,6 +85,8 @@ def recipe_detail(request, slug):
         "comments": comments,
         "comment_count": comment_count,
         "comment_form": comment_form,
+        "likes_count": likes_count,
+        "user_liked": user_liked,
         },
     )
 
@@ -118,4 +128,24 @@ def comment_delete(request, slug, comment_id):
         messages.add_message(request, messages.ERROR, "It is only possible to delete your own comments!")
     
     return HttpResponseRedirect(reverse('recipe_detail', args=[slug]))
+
+
+@login_required
+def like_recipe(request, slug):
+    """
+    View to handle liking/unliking a recipe
+    """
+    recipe = get_object_or_404(Recipe, slug=slug, status=1)
+    like, created = Like.objects.get_or_create(user=request.user, recipe=recipe)
+
+    if not created:
+        # If the Like already exists, remove it (unlike)
+        like.delete()
+        liked = False
+    else:
+        liked = True
+    
+    likes_count = Like.objects.filter(recipe=recipe).count()
+    return JsonResponse({'liked': liked, 'likes_count': likes_count})
+
         
