1f1bafa124dbfdc67488c2113b1e3c41dfe6199c
Sophie Tigerholm <sophie.tigerholm@gmail.com>
2024-09-04 12:48:53 +0000

    Create like model and migrate to database

--- recipes/models.py
+++ recipes/models.py
@@ -54,4 +54,16 @@ class Comment(models.Model):
         return f"Comment {self.body} written by {self.author}"
 
 
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ('user', 'recipe')
+    
+    def __str__(self):
+        return f"{self.user.username} likes {self.recipe.title}"
+
+
 
